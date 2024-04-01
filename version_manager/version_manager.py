c27c6a3ce0acca848c1a49d3a638e1b79af955f6
Cesar Velazquez <cesarve@gmail.com>
2024-03-31 17:47:47 -0700

    Added method to generate thumbnails.

--- version_manager/version_manager.py
+++ version_manager/version_manager.py
@@ -1,7 +1,9 @@
+import os
 import version_manager.utils as utils
 import version_manager.common as common
 from pprint import pprint
 from PyQt5 import QtCore
+from krita import *
 
 
 def doit():
@@ -19,7 +21,7 @@ class VersionManager(QtCore.QObject):
     def doit(self):
         self.add_checkpoint()
 
-    def add_checkpoint(self, msg='', autosave=False):
+    def add_checkpoint(self, msg='', autosave=False, generate_thumbnail=True):
         """Adds document checkpoint to data directory
 
         Parameters:
@@ -41,9 +43,8 @@ class VersionManager(QtCore.QObject):
                     'Auto-Save enabled. Saving document to {}'.format(doc.fileName()))
                 doc.save()
             else:
-                common.message_box(
+                raise Exception(
                     "Document is modified.\nPlease save the current document before creating a checkpoint")
-                return
 
         vmutils = utils.Utils(doc.fileName())
 
@@ -54,5 +55,39 @@ class VersionManager(QtCore.QObject):
                 f'Initializing data directory {vmutils.data_dir}')
             vmutils.init()
 
-        vmutils.add_checkpoint(msg)
+        doc_id, doc_data = vmutils.add_checkpoint(msg)
         pprint(vmutils.history)
+
+        if generate_thumbnail:
+            filename = os.path.join(
+                vmutils.data_dir, doc_data['dirname'], 'thumbnail.png')
+            self.info_update.emit(f'Generating thumbnail: {filename}')
+            self.generate_thumbnail(doc, filename)
+            self.info_update.emit(
+                'Updating document history with thumbnail information')
+            vmutils.lock_history()
+            vmutils.read_history()
+            history = vmutils.history
+            if doc_id not in history:
+                vmutils.unlock_history()
+                raise KeyError(f'document {doc_id} not found in history json')
+            doc_data = history[doc_id]
+            doc_data['thumbnail'] = 'thumbnail.png'
+            vmutils.write_history()
+            vmutils.unlock_history()
+
+    def generate_thumbnail(self, doc, filename):
+        clone = doc.clone()
+        clone.setBatchmode(True)
+        clone.flatten()
+
+        target_dim = 240.0
+        width = clone.width()
+        height = clone.height()
+        max_dim = max(width, height)
+        scale_factor = target_dim/max_dim
+        clone.scaleImage(int(width*scale_factor), int(height*scale_factor),
+                         int(clone.xRes()*scale_factor), int(clone.yRes() * scale_factor), "box")
+        info = InfoObject()
+
+        clone.exportImage(filename, info)
