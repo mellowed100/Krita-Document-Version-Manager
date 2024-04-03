4c1204f946010ba509b5b348b65ae113d1881a82
Cesar Velazquez <cesarve@gmail.com>
2024-04-02 22:14:16 -0700

    Added context menu to table view.

--- version_manager/qt_history_widget.py
+++ version_manager/qt_history_widget.py
@@ -8,6 +8,11 @@ import ast
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
 from . import utils
+from pprint import pprint
+
+
+def doit():
+    print('ttttttt')
 
 
 class HistoryModel(QtCore.QAbstractTableModel):
@@ -15,8 +20,8 @@ class HistoryModel(QtCore.QAbstractTableModel):
         """Table model dor displaying document history.
 
 
-        column 0 = thumbnail
-        column 1 = id (hidden)
+        column 0 = key (hidden)
+        column 1 = thumbnail
         column 2 = date
         column 3 = checkpoint message
 
@@ -32,12 +37,14 @@ class HistoryModel(QtCore.QAbstractTableModel):
         self._history = self._utils.history
 
         # 2D array of data for model
-        self._data = [[self._history[key][property] for property in (
-            'thumbnail', 'dirname', 'date', 'message')] for key in reversed(sorted(self._history))]
-
-        # convert literal to original type
-        for row in self._data:
-            row[3] = ast.literal_eval(row[3])
+        self._data = [
+            [
+                key,
+                self._history[key]['thumbnail'],
+                self._history[key]['date'],
+                ast.literal_eval(self._history[key]['message'])
+            ]
+            for key in reversed(sorted(self._history))]
 
         # Default thumbnail dimension
         self._default_dim = 240
@@ -45,14 +52,29 @@ class HistoryModel(QtCore.QAbstractTableModel):
         # Default thumbnail scaling factor
         self._thumbnail_scale = 1.0
 
+    @property
+    def history(self):
+        """Dictionary holding document version data"""
+        return self._history
+
+    @property
+    def utils(self):
+        """version_manager.Utils instance"""
+        return self._utils
+
     def data(self, index, role):
 
         # thumbnail column
-        if index.column() == 0:
+        if index.column() == 1:
+
+            # get dictionary key for current document in column 0
+            key = self._data[index.row()][0]
 
             # build path to thumbnail
             pixmap_filename = os.path.join(
-                self._utils.data_dir, self._data[index.row()][1], self._data[index.row()][0])
+                self._utils.data_dir,
+                self._history[key]['dirname'],
+                self._history[key]['thumbnail'])
 
             # quit if not file found
             if not os.path.isfile(pixmap_filename):
@@ -113,10 +135,13 @@ class HistoryWidget(QtWidgets.QWidget):
         self.table.horizontalHeader().hide()
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.resizeRowsToContents()
-        self.table.resizeColumnToContents(0)
-        self.table.hideColumn(1)
+        self.table.resizeColumnToContents(1)  # thumbnail column
+        self.table.hideColumn(0)  # key column
         self.table.setSelectionMode(QtWidgets.QAbstractItemView.NoSelection)
         self.table.setAlternatingRowColors(True)
+        self.table.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+        self.table.customContextMenuRequested.connect(self.context_menu)
+        # self.table = HistoryTable(self)
 
         layout.addWidget(self.table)
 
@@ -127,16 +152,92 @@ class HistoryWidget(QtWidgets.QWidget):
         self.slider_widget.setValue(64)
         layout.addWidget(self.slider_widget)
 
+        self.context_menu = QtWidgets.QMenu(self)
+
+        self.context_menu_actions = {
+            'Edit Checkpoint Message': 'edit_message',
+            'Generate Thumbnail': 'generate_thumbnail',
+            'Make Active': 'make_active',
+            'Delete Checkpoint': 'delete_checkpoint'}
+
+        for desc in self.context_menu_actions:
+            self.context_menu.addAction(QtWidgets.QAction(desc, self))
+
+    def context_menu(self, pos):
+        """Display context menu in the history table"""
+
+        # get document id for selected row
+        row = self.table.verticalHeader().logicalIndexAt(pos)
+        index = self.model.index(row, 0)
+        doc_id = self.model.data(index, Qt.DisplayRole)
+
+        # get context menu selection
+        result = self.context_menu.exec_(QtGui.QCursor.pos())
+
+        # call menu item's method and pass document id
+        getattr(self, self.context_menu_actions[result.text()])(doc_id)
+
+    def edit_message(self, doc_id):
+        """Opens dialog box to edit checkpoint message.
+
+        Parameters:
+        doc_id (str): document key in history dictionary
+        """
+
+        # get current message
+        msg = self.model.history[doc_id]['message']
+
+        editor = QtWidgets.QDialog(self)
+
+        # create text box to edit checkpoint message
+        text_box = QtWidgets.QPlainTextEdit()
+        text_box.appendPlainText(ast.literal_eval(msg))
+
+        # create ok/cancel buttons
+        buttons = QtWidgets.QDialogButtonBox(QtWidgets.QDialogButtonBox.Ok |
+                                             QtWidgets.QDialogButtonBox.Cancel)
+        buttons.accepted.connect(editor.accept)
+        buttons.rejected.connect(editor.reject)
+
+        # assemble widgets
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(text_box)
+        layout.addWidget(buttons)
+        editor.setLayout(layout)
+
+        # show window and get result
+        result = editor.exec()
+        if not result:
+            return
+
+        # get new checkpoint message
+        new_msg = text_box.toPlainText()
+
+        # check if msg is unchanged
+        if msg == repr(new_msg):
+            return
+
+        # commit new message to disk
+        self.model.utils.update_checkpoint_message(doc_id, new_msg)
+        self.reload_history()
+
+    def make_active(self, event):
+        print('in make active')
+
+    def generate_thumbnail(self, event):
+        print('in generate thumbnail')
+
+    def delete_checkpoint(self, doc_id):
+        pass
+
     def set_default_icon_scale(self):
         self.slider_widget.setValue(64)
         self.resize_thumbnails(64)
 
-    def reload_history(self, doc):
-        """Rebuilds model from document version history
+    def reload_history(self):
+        """Rebuilds model from current document version history"""
 
-        Parameters:
-            doc (Krita.Document) - Document to get history from.
-        """
+        doc = Krita.instance().activeDocument()
 
         # remove existing model
         self.table.setModel(None)
@@ -163,8 +264,8 @@ class HistoryWidget(QtWidgets.QWidget):
         self.table.horizontalHeader().hide()
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.resizeRowsToContents()
-        self.table.resizeColumnToContents(0)
-        self.table.hideColumn(1)
+        self.table.resizeColumnToContents(1)
+        self.table.hideColumn(0)
         self.resize_thumbnails(self.slider_widget.value())
 
         self.table.setEnabled(True)
@@ -184,7 +285,7 @@ class HistoryWidget(QtWidgets.QWidget):
         self.model.setThumbnailScale(float(s)/self.model._default_dim)
 
         self.table.resizeRowsToContents()
-        self.table.resizeColumnToContents(0)
+        self.table.resizeColumnToContents(1)
 
     def setColumnCount(self, n):
         '''Does nothing. Included here to be compatible with Designer generated gui'''
